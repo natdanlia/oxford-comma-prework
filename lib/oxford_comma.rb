37370c42af37ecfec5d3869f2d5cb13b07596ca3
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-05 19:10:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array[0]
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length > 2
+     last_item = array.pop
+    sentence =  array.join(', ')
+    sentence << ", and #{last_item}"
+  end
 end
